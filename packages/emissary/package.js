7f5cb36fd4b868781d0d559d095fe204d7c5fdb3
Jason Raede <jason.raede@gmail.com>
2015-09-24 11:53:26 -0400

    Deploying version 0.2.0

--- packages/emissary/package.js
+++ packages/emissary/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dispatch:emissary',
   summary: 'Extensible/configurable notifications package',
-  version: '0.1.3'
+  version: '0.2.0'
 });
 
 Package.onUse(function (api) {
